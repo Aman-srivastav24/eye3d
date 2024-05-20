cde3281f543345e683d83251e36c678672da68b7
unknown <shinchanmusic24@gmail.com>
2024-05-20 13:59:22 +0530

    pointer1

--- public/Eye.jsx
+++ public/Eye.jsx
@@ -10,7 +10,7 @@ export default function Model(props) {
   const { gl, camera, scene } = useThree();
   const [intersectedPoint, setIntersectedPoint] = useState(null);
   const cornea =[0.5,0.5,-1];
-  const retina = [0.8,-1.37075,.9]
+  const retina = [0.8,-1,-1.9]
 
   const raycaster = new THREE.Raycaster();
   const mouse = new THREE.Vector2();
@@ -47,25 +47,25 @@ export default function Model(props) {
       onPointerDown={handleClick}
     >
       <group name="Scene">
-        <group name="Empty" rotation={[Math.PI, 0, Math.PI]} scale={5.6} />
-        <mesh name="object_0" geometry={nodes.object_0.geometry} material={materials.object_0} rotation={[Math.PI / 2, 0, 0]} scale={2} />
-        <mesh name="object_1" geometry={nodes.object_1.geometry} material={materials.object_1} rotation={[Math.PI / 2, 0, 0]} scale={2} />
-        <mesh name="object_2" geometry={nodes.object_2.geometry} material={materials.object_2} rotation={[Math.PI / 2, 0, 0]} scale={2} />
-        <mesh name="object_3" geometry={nodes.object_3.geometry} material={materials.object_3} rotation={[Math.PI / 2, 0, 0]} scale={2} />
-        <mesh name="object_4" geometry={nodes.object_4.geometry} material={materials.object_4} rotation={[Math.PI / 2, 0, 0]} scale={2} />
-        <mesh name="object_6" geometry={nodes.object_6.geometry} material={materials.object_6} rotation={[Math.PI / 2, 0, 0]} scale={2} />
-        <mesh name="object_7" geometry={nodes.object_7.geometry} material={materials['Procedural Glass for Windows']} rotation={[Math.PI / 2, 0, 0]} scale={2} />
-        <mesh name="object_8" geometry={nodes.object_8.geometry} material={materials.object_8} rotation={[Math.PI / 2, 0, 0]} scale={2} />
-        <mesh name="object_9" geometry={nodes.object_9.geometry} material={materials.object_9} rotation={[Math.PI / 2, 0, 0]} scale={2} />
-        <mesh name="object_10" geometry={nodes.object_10.geometry} material={materials.object_10} rotation={[Math.PI / 2, 0, 0]} scale={2} />
-        <mesh name="object_11" geometry={nodes.object_11.geometry} material={materials.object_11} rotation={[Math.PI / 2, 0, 0]} scale={2} />
-        <mesh name="object_12" geometry={nodes.object_12.geometry} material={materials.object_12} rotation={[Math.PI / 2, 0, 0]} scale={2} />
-        <mesh name="object_13" geometry={nodes.object_13.geometry} material={materials.object_13} rotation={[Math.PI / 2, 0, 0]} scale={2} />
-        <mesh name="object_14" geometry={nodes.object_14.geometry} material={materials.object_14} rotation={[Math.PI / 2, 0, 0]} scale={2} />
-        <mesh name="object_15" geometry={nodes.object_15.geometry} material={materials.object_15} rotation={[Math.PI / 2, 0, 0]} scale={2} />
-        <mesh name="Plane003" geometry={nodes.Plane003.geometry} material={materials['Material.008']} position={[0, 0.002, 0.009]} rotation={[1.505, 0, 0]} scale={1.6} />
-        <mesh name="Plane009" geometry={nodes.Plane009.geometry} material={materials.Head} position={[-0.002, -0.001, 0.01]} rotation={[1.505, 0, 0]} scale={1.6} />
-        <mesh name="Cube" geometry={nodes.Cube.geometry} material={materials['Material.003']} position={[0.008, 0.007, -0.004]} rotation={[Math.PI, 0, -2.823]} scale={[1.2, 2, 2]} />
+        <group name="Empty" rotation={[Math.PI, 0, Math.PI]} scale={2.8} />
+        <mesh name="object_0" geometry={nodes.object_0.geometry} material={materials.object_0} rotation={[Math.PI / 2, 0, 0]} scale={1} />
+        <mesh name="object_1" geometry={nodes.object_1.geometry} material={materials.object_1} rotation={[Math.PI / 2, 0, 0]} scale={1} />
+        <mesh name="object_2" geometry={nodes.object_2.geometry} material={materials.object_2} rotation={[Math.PI / 2, 0, 0]} scale={1} />
+        <mesh name="object_3" geometry={nodes.object_3.geometry} material={materials.object_3} rotation={[Math.PI / 2, 0, 0]} scale={1} />
+        <mesh name="object_4" geometry={nodes.object_4.geometry} material={materials.object_4} rotation={[Math.PI / 2, 0, 0]} scale={1} />
+        <mesh name="object_6" geometry={nodes.object_6.geometry} material={materials.object_6} rotation={[Math.PI / 2, 0, 0]} scale={1} />
+        <mesh name="object_7" geometry={nodes.object_7.geometry} material={materials['Procedural Glass for Windows']} rotation={[Math.PI / 2, 0, 0]} scale={1} />
+        <mesh name="object_8" geometry={nodes.object_8.geometry} material={materials.object_8} rotation={[Math.PI / 2, 0, 0]} scale={1} />
+        <mesh name="object_9" geometry={nodes.object_9.geometry} material={materials.object_9} rotation={[Math.PI / 2, 0, 0]} scale={1} />
+        <mesh name="object_10" geometry={nodes.object_10.geometry} material={materials.object_10} rotation={[Math.PI / 2, 0, 0]} scale={1} />
+        <mesh name="object_11" geometry={nodes.object_11.geometry} material={materials.object_11} rotation={[Math.PI / 2, 0, 0]} scale={1} />
+        <mesh name="object_12" geometry={nodes.object_12.geometry} material={materials.object_12} rotation={[Math.PI / 2, 0, 0]} scale={1} />
+        <mesh name="object_13" geometry={nodes.object_13.geometry} material={materials.object_13} rotation={[Math.PI / 2, 0, 0]} scale={1} />
+        <mesh name="object_14" geometry={nodes.object_14.geometry} material={materials.object_14} rotation={[Math.PI / 2, 0, 0]} scale={1} />
+        <mesh name="object_15" geometry={nodes.object_15.geometry} material={materials.object_15} rotation={[Math.PI / 2, 0, 0]} scale={1} />
+        <mesh name="Plane003" geometry={nodes.Plane003.geometry} material={materials['Material.008']} position={[0, 0.002, 0.009]} rotation={[1.505, 0, 0]} scale={0.8} />
+        <mesh name="Plane009" geometry={nodes.Plane009.geometry} material={materials.Head} position={[-0.002, -0.001, 0.01]} rotation={[1.505, 0, 0]} scale={0.8} />
+        <mesh name="Cube" geometry={nodes.Cube.geometry} material={materials['Material.003']} position={[0.008, 0.007, -0.004]} rotation={[Math.PI, 0, -2.823]} scale={[0.6, 1, 1]} />
       </group>
       <Html position={cornea} distanceFactor={10}>
         <div className="label" title='Cornea'>c</div>
